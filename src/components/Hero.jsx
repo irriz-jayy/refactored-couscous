fdd00c193701d446a6662e710bb2e24eb590229f
irriz-jayy <jayfelix789@gmail.com>
2024-01-23 11:24:17 +0300

    added responsiveness

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,10 +4,10 @@ const Hero = () => {
   return (
     <>
       {/* container */}
-      <div className="border-4 border-red-800 h-[80vh] flex">
+      <div className="border-4 border-red-800 min-h-[80vh] md:h-[80vh] flex flex-col  md:flex-row">
         {/* image and text containers */}
         {/* text container */}
-        <div className="w-1/2 border flex flex-col items-center justify-center gap-4">
+        <div className="w-full h-1/2 md:w-1/2 md:h-full border flex flex-col items-center justify-center gap-4">
           <div className="w-3/4 border text-4xl">
             Lorem ipsum dolor sit amet, consectetur adipisicing.
           </div>
@@ -18,10 +18,13 @@ const Hero = () => {
             aperiam. Architecto, aperiam voluptatum.
           </div>
           {/* CTA button */}
-          <button className="border h-12 w-24">CTA</button>
+          <button className="border h-12 w-24 m-2">CTA</button>
         </div>
         {/* image container */}
-        <img src="../assets/hero.jpg" className="w-1/2 border object-contain" />
+        <img
+          src="../assets/hero.jpg"
+          className="w-full h-1/2 md:w-1/2 md:h-full border object-contain"
+        />
       </div>
     </>
   );
